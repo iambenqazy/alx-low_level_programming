03210eba969d46d9adecb58fcb3f012a00901378
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-15 21:57:45 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - print _putchar
- * is written by mbah
+ * is written by bko
  * Return: 0
  */
 
-int main(void)
-{
-	putchar('');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+int _putchar(char c) {
+    return putchar(c);
 }
 
+int main() {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return 0;
+}
