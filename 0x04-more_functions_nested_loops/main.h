3627fa5910a126631abd8c08013446344e7d0313
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-17 23:55:34 +0000

    print numbers most

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
+
+#endif /* MAIN_H */
