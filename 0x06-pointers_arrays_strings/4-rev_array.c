6eaa6e50be3a46f4563f723415238baba359dc45
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-23 20:37:30 +0000

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+*  _strcmp - Function which compare two strings and
+*@s1: first string
+*@s2:second string
+*Return:
+*		returns zero if s1 == s2
+*		returns negative number if s1 < s2
+*		returns positive number if s1 > s2
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, diff = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			diff = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+
+	}
+	return (diff);
+}
+0x06-pointers_arrays_strings $ cat 4-rev_array.c
+#include "main.h"
+
+/**
+*  reverse_array - function that reverse content of array
+*  @a: array
+*  @n: number of elements
+*/
+
+void reverse_array(int *a, int n)
+{
+int tmp, index;
+for (index = n - 1; index > n / 2; index--)
+{
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
+}
+}
