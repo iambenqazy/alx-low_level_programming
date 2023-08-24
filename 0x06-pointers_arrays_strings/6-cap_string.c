dd8b95d53b374f8df5afa44e3a6dc9d307aa23e0
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-24 21:57:27 +0000

    corrections

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,24 @@
  */
 char *cap_string(char *str)
 {
-    int index = 0;
+	int index = 0;
 
-    while (str[index])
-    {
-        if (str[index] >= 'a' && str[index] <= 'z')
-        {
-            if (index == 0 ||
-                str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' ||
-                str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' ||
-                str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' ||
-                str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' ||
-                str[index - 1] == '}')
-            {
-                str[index] -= 32;
-            }
-        }
-        index++;
-    }
-    return (str);
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			if (index == 0 ||
+				str[index - 1] == ' ' || str[index - 1] == '\t' ||
+				str[index - 1] == '\n' || str[index - 1] == ',' ||
+				str[index - 1] == ';' || str[index - 1] == '.' ||
+				str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' ||
+				str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			{
+				str[index] -= 32;
+			}
+		}
+		index++;
+	}
+	return (str);
 }
