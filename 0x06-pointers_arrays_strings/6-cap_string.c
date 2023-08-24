12cb5c0e9f87edea0c60e1cc08251740a0d486da
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-24 21:45:38 +0000

    corrections

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,29 @@
 #include "main.h"
+
 /**
-*cap_string - function that capitalize first character of a word
-*@str: string to capitalize
-*Return:returns the capitalized string
-*/
+ * cap_string - function that capitalizes the first character of a word
+ * @str: string to capitalize
+ * Return: returns the capitalized string
+ */
 char *cap_string(char *str)
 {
-	int index = 0;
+    int index = 0;
 
-	while (str[++index])
-	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
-	}
-	return (str);
+    while (str[index])
+    {
+        if (str[index] >= 'a' && str[index] <= 'z')
+        {
+            if (index == 0 ||
+                str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' ||
+                str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' ||
+                str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' ||
+                str[index - 1] == '(' || str[index - 1] == ')' || str[index - 1] == '{' ||
+                str[index - 1] == '}')
+            {
+                str[index] -= 32;
+            }
+        }
+        index++;
+    }
+    return (str);
 }
