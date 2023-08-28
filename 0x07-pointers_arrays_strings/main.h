4f89a3e661d5827d423f0f72a1732d83de95d13a
Kwame Benqazy <iambenqazy@gmail.com>
2023-08-28 12:34:36 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
