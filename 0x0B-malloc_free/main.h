393fd90c5512207ffeedb59fea75dd35f5cc6491
Kwame Benqazy <iambenqazy@gmail.com>
2023-09-05 15:28:42 +0000

    alloc grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,7 +7,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int len(char *str);
 char *str_concat(char *s1, char *s2);
-
+int **alloc_grid(int width, int height);
 
 
 
