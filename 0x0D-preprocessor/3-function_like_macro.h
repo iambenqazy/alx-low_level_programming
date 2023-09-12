0e55baad07f9975e2a16c05a0375291bc921c2eb
Kwame Benqazy <iambenqazy@gmail.com>
2023-09-12 19:48:40 +0000

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,3 @@
-#include <stdio.h>
-
-/**
-*main - prints the name of
-*the file it was compiled from
-*
-*Return: nothing to be returned
-*/
-
-int main(void)
-{
-printf("%s\n", __FILE__);
-return (0);
-}
-0x0D-preprocessor $ cat 3-function_like_macro.h
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
