baac72a3b4db05e84cb5bd90a575a74ea358e21c
Kwame Benqazy <iambenqazy@gmail.com>
2023-09-14 17:10:18 +0000

    print strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,7 +12,7 @@ void (*print)(va_list arg);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 
 
 
