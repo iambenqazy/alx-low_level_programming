2b915d4b7ce6e1ce175124d562f631e0aa2ab5db
Kwame Benqazy <iambenqazy@gmail.com>
2023-09-27 00:08:11 +0000

    code fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,36 +10,30 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-    listint_t *slow, *fast, *temp;
-    size_t node_count = 0;
+listint_t *slow, *fast, *temp;
+size_t node_count = 0;
 
-    slow = fast = *h;
+slow = fast = *h;
 
-    while (slow != NULL && fast != NULL)
-    {
-        slow = slow->next;
-        fast = (fast->next != NULL) ? fast->next->next : NULL;
+while (slow != NULL && fast != NULL)
+{
+slow = slow->next;
+fast = (fast->next != NULL) ? fast->next->next : NULL;
+if (fast != NULL && fast == slow)
+{
+fprintf(stderr, "Infinite loop detected\n");
+exit(98);
+}
+if (slow != NULL)
+{
+temp = slow;
+slow = slow->next;
+free(temp);
+node_count++;
+}
+}
+*h = NULL;
 
-        /* Check if there's a loop (fast pointer catches up to slow pointer) */
-        if (fast != NULL && fast == slow)
-        {
-            fprintf(stderr, "Infinite loop detected\n");
-            exit(98);
-        }
-
-        /* Free the nodes as you go */
-        if (slow != NULL)
-        {
-            temp = slow;
-            slow = slow->next;
-            free(temp);
-            node_count++;
-        }
-    }
-
-    /* Set the head pointer to NULL */
-    *h = NULL;
-
-    return node_count;
+return (node_count);
 }
 
